319161c1896c50829ebfcc05c9500398ec4d2e4b
Suliman Ibrahim <suibrahi@lab3r8s10.42abudhabi.ae>
2024-03-06 06:59:57 +0400

    editing the parsing part (pipes)

--- parsing.c
+++ parsing.c
@@ -3,15 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aken <aken@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: suibrahi <suibrahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 20:29:36 by suibrahi          #+#    #+#             */
-/*   Updated: 2024/03/03 05:23:36 by suibrahi         ###   ########.fr       */
-/*                                                    ###   ########.fr       */
+/*   Updated: 2024/03/06 06:58:28 by suibrahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	skip_quotes(t_input *input, int *i, int q_type)
+{
+	if (input->cmds[*i] == q_type)
+		(*i)++;
+	while (input->cmds[*i] != q_type && input->cmds[*i])
+		(*i)++;
+}
 
 bool	pipe_parsing(t_input *input, t_var *var)
 {
@@ -21,9 +28,13 @@ bool	pipe_parsing(t_input *input, t_var *var)
 		while (input->cmds[var->i] == ' ')
 			var->i++;
 		if (input->cmds[var->i] == PIPE)
-			return (printf("Syntax error : unexpected pipe \n"), false);
+			return (printf("Syntax error : unexpected pipe 1\n"), false);
 		while (input->cmds[var->i])
 		{
+			if (input->cmds[var->i] == DOUBLE_QUOTE)
+				skip_quotes(input, &var->i, DOUBLE_QUOTE);
+			if (input->cmds[var->i] == SINGLE_QUOTE)
+				skip_quotes(input, &var->i, SINGLE_QUOTE);
 			if (input->cmds[var->i++] == PIPE)
 			{
 				input->num_of_cmd++;
