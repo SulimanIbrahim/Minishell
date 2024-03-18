9b62a1be155e56bcc949b110cc25d0fc1a7debfb
aken20 <teentaiten@gmail.com>
2024-03-18 11:23:23 +0400

    fixing-environment-variables

--- replace_env_vars.c
+++ replace_env_vars.c
@@ -3,98 +3,124 @@
 /*                                                        :::      ::::::::   */
 /*   replace_env_vars.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aken <aken@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ahibrahi <ahibrahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 23:43:11 by aken              #+#    #+#             */
-/*   Updated: 2024/03/16 05:41:56 by aken             ###   ########.fr       */
+/*   Updated: 2024/03/18 08:34:07 by ahibrahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_remove_wrong_var(t_input *input, t_var *var)
+char	*env_srch(char *cmd)
 {
-	int		len_befor;
-	int		len_after;
-	char	*n_s;
+	int	i;
 
-	var->i = 0;
-	var->j = 0;
-	while (input->cmds[var->i] && input->cmds[var->i] != '$')
-		var->i++;
-	len_befor = var->i;
-	while (input->cmds[var->i] && input->cmds[var->i] != ' ')
-		var->i++;
-	var->n = var->i;
-	while (input->cmds[var->i++])
-		var->j++;
-	len_after = var->j;
-	n_s = malloc(len_befor + len_after + 1);
-	if (!n_s)
-		return ;
-	ft_strlcpy(n_s, input->cmds, len_befor + 1);
-	ft_strlcpy(n_s + len_befor, input->cmds + var->n, len_after + 1);
-	var->temp = input->cmds;
-	free(var->temp);
-	input->cmds = n_s;
+	i = 0;
+	if (!cmd)
+		return (NULL);
+	while (cmd[i])
+	{
+		if (cmd[i] == '\'' || cmd[i] == '"')
+			skip_quotes(cmd, &i, cmd[i]);
+		if (cmd[i] && cmd[i] == '$' && cmd[i + 1] && cmd[i + 1] != ' ')
+			return (cmd + i);
+		else if (!cmd[i])
+			return (NULL);
+		i++;
+	}
+	return (NULL);
 }
 
-void	ft_replace_env(t_input *input, char *env, t_var *var)
+void	ft_remove_wrong_var(t_input *input)
 {
-	int		len_befor;
-	int		len_after;
-	char	*n_s;
+	t_var	var;
+	char	*new_line;
 
-	var->c = 0;
-	var->n = 0;
-	var->len = ft_strlen(ft_strchr(env, '=') + 1);
-	while (input->cmds[var->c] && input->cmds[var->c] != '$')
-		var->c++;
-	len_befor = var->c;
-	while (input->cmds[var->c] && input->cmds[var->c] != ' ')
-		var->c++;
-	var->n = var->c;
-	while (input->cmds[var->c++])
-		var->n++;
-	len_after = var->n;
-	n_s = malloc(len_befor + len_after + var->len + 1);
-	if (!n_s)
+	var.i = 0;
+	var.j = 0;
+	while (input->cmds[var.i] && input->cmds[var.i] != '$')
+		var.i++;
+	var.c = var.i;
+	while (input->cmds[var.i] && input->cmds[var.i] != ' ')
+		var.i++;
+	var.n = var.i;
+	while (input->cmds[var.i++])
+		var.j++;
+	var.n = var.j;
+	new_line = malloc(var.c + var.n + 1);
+	if (!new_line)
 		return ;
-	ft_strlcpy(n_s, input->cmds, len_befor + 2);
-	ft_strlcpy(n_s + len_befor, ft_strchr(env, '=') + 1, var->len + 1);
-	ft_strlcpy(n_s + len_befor + var->len, input->cmds + var->n, len_after + 1);
-	var->temp = input->cmds;
-	free(var->temp);
-	input->cmds = n_s;
+	ft_strlcpy(new_line, input->cmds, var.c + 1);
+	ft_strlcpy(new_line + var.c, input->cmds + var.n, var.n + 1);
+	var.temp = input->cmds;
+	free(var.temp);
+	input->cmds = new_line;
+}
+
+void	ft_replace_env(t_input *input, char *env)
+{
+	t_var	var;
+	char	*new_line;
+
+	var.c = 0;
+	var.n = 0;
+	var.len = ft_strlen(ft_strchr(env, '=') + 1);
+	while (input->cmds[var.c] && input->cmds[var.c] != '$')
+		var.c++;
+	var.i = var.c;
+	while (input->cmds[var.c] && input->cmds[var.c] != ' ')
+		var.c++;
+	var.n = var.c;
+	while (input->cmds[var.c++])
+		var.n++;
+	var.j = var.n;
+	new_line = malloc(var.i + var.j + var.len + 1);
+	if (!new_line)
+		return ;
+	ft_strlcpy(new_line, input->cmds, var.i + 2);
+	ft_strlcpy(new_line + var.i, ft_strchr(env, '=') + 1, var.len + 1);
+	ft_strlcpy(new_line + var.i + var.len, input->cmds + var.n, var.j + 1);
+	var.temp = input->cmds;
+	free(var.temp);
+	input->cmds = new_line;
 }
 
 void	ft_check_env(t_input *input, t_var *var)
 {
-	char	*p;
-
 	var->i = 0;
 	var->j = 0;
-	if (!input->cmds[var->i] || !ft_strchr(input->cmds, '$'))
+	if (!input->cmds || !env_srch(input->cmds))
 		return ;
-	p = ft_strchr(input->cmds, '$') + 1;
-	if (!p || p[var->i] == '(')
-		return ;
-	while (input->env[var->i])
+	printf("%s\n", );
+	while (env_srch(input->cmds))
 	{
-		if (input->env[var->i][var->j] == p[var->j])
-		{
-			while (p[var->j] && input->env[var->i][var->j] && p[var->j] != ' '
-				&& p[var->j] == input->env[var->i][var->j])
-				var->j++;
-			if (input->env[var->i][var->j] == '=' && (!p[var->j]
-				|| p[var->j] == ' '))
-				ft_replace_env(input, input->env[var->i], var);
-		}
-		if (!ft_strchr(input->cmds, '$'))
+		var->temp = env_srch(input->cmds) + 1;
+		if (!var->temp)
 			return ;
-		p = ft_strchr(input->cmds, '$') + 1;
-		var->j = 0;
-		var->i++;
+		while (input->env[var->i])
+		{
+			if (input->env[var->i][var->j] == var->temp[var->j])
+			{
+				while (var->temp[var->j] && input->env[var->i][var->j]
+					&& var->temp[var->j] == input->env[var->i][var->j])
+					var->j++;
+				if (input->env[var->i][var->j] == '=' && (!var->temp[var->j]
+					|| var->temp[var->j] == ' '))
+				{
+					ft_replace_env(input, input->env[var->i]);
+					if (!env_srch(input->cmds))
+						break ;
+					var->temp = env_srch(input->cmds) + 1;
+				}
+			}
+			var->j = 0;
+			var->i++;
+		}
+		if (!env_srch(input->cmds))
+			break ;
+		if (var->temp == env_srch(input->cmds) + 1)
+			ft_remove_wrong_var(input);
+		var->i = 0;
 	}
-	ft_remove_wrong_var(input, var);
 }
